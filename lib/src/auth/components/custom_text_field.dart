72b54cc95fa584b81078bfa6a7aa58156ec93771
Watila Costa <watila.costa@redealumni.com>
2022-12-04 22:23:18 -0300

    telas de login , sing-up e base

--- lib/src/auth/components/custom_text_field.dart
+++ lib/src/auth/components/custom_text_field.dart
@@ -1,33 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final IconData prefixIcon;
   final IconData? suffixIcon;
   final String label;
-  final bool occultText;
+  final bool isSecret;
+  final List<TextInputFormatter>? inputFormatters;
 
   const CustomTextField({
     Key? key,
     required this.prefixIcon,
     required this.label,
     this.suffixIcon,
-    this.occultText = false,
+    this.inputFormatters,
+    this.isSecret = false,
   }) : super(key: key);
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool isObscure = false;
+
+  @override
+  void initState() {
+    isObscure = widget.isSecret;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 8.0),
       child: TextFormField(
-        obscureText: occultText,
+        inputFormatters: widget.inputFormatters,
+        obscureText: isObscure,
         decoration: InputDecoration(
           isDense: true,
-          prefixIcon: Icon(prefixIcon, size: 20,),
-          suffixIcon: occultText ? IconButton(
-            onPressed: null,
-            icon: Icon(suffixIcon, size: 20,)
+          prefixIcon: Icon(widget.prefixIcon, size: 20,),
+          suffixIcon: widget.isSecret ? IconButton(
+            onPressed: () {
+              setState(() {
+                isObscure = !isObscure;
+              });
+            },
+            icon: Icon(isObscure ? Icons.visibility : Icons.visibility_off, size: 20,)
           ) : null,
-          labelText: label,
+          labelText: widget.label,
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(18)),
         ),
       ),
---
+++ lib/src/auth/sign_up_screen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:kitanda/src/auth/components/custom_text_field.dart';
+import 'package:kitanda/src/config/custom_colors.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+class SignUpScreen extends StatelessWidget {
+  SignUpScreen({Key? key}) : super(key: key);
+
+  final cpfFormatter = MaskTextInputFormatter(
+    mask: '###.###.###-##',
+    filter: {'#': RegExp(r'[0-9]')},
+  );
+
+  final phoneFormatter = MaskTextInputFormatter(
+    mask: '## # ####-####',
+    filter: {'#': RegExp(r'[0-9]')},
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Stack(
+            children: [
+              Positioned(
+                left: 10,
+                top: 10,
+                child: SafeArea(
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back,
+                    ),
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  const Expanded(
+                    child: Center(
+                      child: Text(
+                        'Cadastro',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 40,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(30),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const CustomTextField(
+                          prefixIcon: Icons.email,
+                          label: 'Email',
+                        ),
+                        const CustomTextField(
+                          prefixIcon: Icons.lock,
+                          label: 'Senha',
+                          isSecret: true,
+                        ),
+                        const CustomTextField(
+                          prefixIcon: Icons.person,
+                          label: 'Nome',
+                        ),
+                        CustomTextField(
+                          prefixIcon: Icons.phone,
+                          label: 'Celular',
+                          inputFormatters: [phoneFormatter],
+                        ),
+                        CustomTextField(
+                          prefixIcon: Icons.file_copy,
+                          label: 'CPF',
+                          inputFormatters: [cpfFormatter],
+                        ),
+                        SizedBox(
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18),
+                              ),
+                            ),
+                            child: const Text('Cadastrar usuário'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
